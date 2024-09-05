3f9929d73cc2e3df7921cce19f5abd64651332ed
Kanizo11 <160481545+Kanizo11@users.noreply.github.com>
2024-09-05 21:02:32 +0200

    Update Sudoku_Solver.cpp

--- Sudoku_Solver.cpp
+++ Sudoku_Solver.cpp
@@ -1,328 +1,268 @@
 #include <iostream>
-#include <forward_list> 
 #include <vector>
-#include <cmath> 
-#include <sstream> 
 
 using namespace std;
-//global variables
-bool isSolved = false;
-vector<vector<int>> mySudoku;
-int sudokuSize=16;
 
-class Sudoku16x16
-{
+class Sudoku9x9 {
 public:
-    int getSudWidth() const {
-        return sudWidth;
+    //vector board is a 9x9 array of ints
+    vector<vector<int>> board;
+
+    //constructor
+    Sudoku9x9(vector<vector<int>> board) {
+        this->board = board;
     }
 
-    void setSudWidth(int sudWidth) {
-        Sudoku16x16::sudWidth = sudWidth;
-    }
-
-    int getCurrNum() const {
-        return currNum;
-    }
-
-    void setCurrNum(int currNum) {
-        Sudoku16x16::currNum = currNum;
-    }
-
-    int getSudHeight() const {
-        return sudHeight;
-    }
-
-    void setSudHeight(int sudHeight) {
-        Sudoku16x16::sudHeight = sudHeight;
-    }
-
-    int getDimension() const {
-        return dimension;
-    }
-
-    void setDimension(int dimension) {
-        Sudoku16x16::dimension = dimension;
-    }
-
-    const forward_list<int> &getQuickSud() const {
-        return quickSud;
-    }
-
-    void setQuickSud(const forward_list<int> &quickSud) {
-        Sudoku16x16::quickSud = quickSud;
-    }
-
-private:
-    class Root;
-    int  sudWidth = 16;
-    int currNum;
-
-
-    static char intXchar(int c){
-        if(c<10){
-            return char(c+48);
-        }
-        else{
-            return char(c+55);
-        }
-    }
-
-
-    class node
-    {
-    public:
-        int row;
-        Root *head;
-        node *up, *down, *left, *right;
-    };
-
-
-    class Root : public node
-    {
-    public:
-        int numOfNodes;
-        Root *left, *right;
-    };
-
-    vector<Root> onY;
-
-    int  sudHeight;
-    int dimension;
-
-    Root *rootLink;
-
-    vector<vector<node>>onX;
-
-
-public:
-
-    Sudoku16x16()
-    {
-        dimension = sudokuSize;
-        currNum = 16;
-        sudHeight = (currNum * dimension * 4) + 1;
-
-        
-        sudWidth = (currNum * 16 ) * currNum;
-
-        onX  = vector<vector<node>>(sudWidth, vector<node>(4, node()));
-        
-        onY= vector<Root>(sudHeight, Root());
-
-        rootLink = &(onY[sudHeight - 1]);
-
-        for (int i = 0; i < sudHeight; i++) 
-        {
-
-            onY[i].up = &onY[i];
-
-            onY[i].down = &onY[i];
-
-            onY[i].left = &(onY[(i + sudHeight - 1) % sudHeight]);
-
-            onY[i].right = &(onY[(i + 1) % sudHeight]);
-
-            onY[i].numOfNodes = 0;
-        }
-
-        int _size = dimension * currNum;
-        int x_expansion =sqrt(currNum);
-        int vertices;
-
-        for (int i = 0; i < sudWidth; i++){
-
-            int theeNum = i % currNum;
-            int currRow = i / (currNum * dimension);
-            int currCol = (i / currNum) % dimension;
-
-            if (mySudoku[currRow][currCol] != 0 && mySudoku[currRow][currCol] != theeNum + 1)
-                continue;
-
-            for (int myTest = 0; myTest < 4; myTest++)
-            {
-                if(myTest == 0){
-                    vertices = i / currNum;
-                } else if(myTest == 1){
-                    vertices = _size + (i % currNum) + (currCol * currNum);
-                } else if(myTest == 2){
-                    vertices = (2 * _size) + (currRow * currNum) + (i % currNum);
-                } else if(myTest == 3){
-                    vertices = (3 * _size) + (i % currNum) + (currRow / x_expansion) * x_expansion * currNum + (currCol / x_expansion) * currNum;
-                } else{
-                    //do nothing baby...!
-                }
-
-
-                onX[i][myTest].row = i;
-                onX[i][myTest].down = &onY[vertices];
-
-                int val1 =1;
-                int val2 =4;
-                onX[i][myTest].right = &(onX[i][(myTest + val1) % val2]);
-                val1 = 3;
-                onX[i][myTest].left = &(onX[i][(myTest + val1) % val2]);
-
-                onX[i][myTest].up = onY[vertices].up;
-
-                onX[i][myTest].head = &onY[vertices];
-
-                onX[i][myTest].up->down = &onX[i][myTest];
-
-                onX[i][myTest].down = &onY[vertices];
-
-                onY[vertices].up->down = &(onX[i][myTest]);
-
-                onY[vertices].up = &(onX[i][myTest]);
-
-
-                onY[vertices].numOfNodes++; 
+    void takeInput() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                cin >> board[i][j];
             }
         }
-    };
-    forward_list<int> quickSud;
+    }
 
-    void printBoard()
-    {
-        int row_now;
-        int col_now;
-        int magicVal;
+    Sudoku9x9() {
+        this->board = vector<vector<int>>(9, vector<int>(9, 0));
+    }
 
-        for (int &r : quickSud)
-        {
-
-            row_now = r / (dimension * dimension);
-            col_now = (r / dimension) % dimension;
-            magicVal = r % dimension + 1;
-
-            mySudoku[row_now][col_now] = magicVal;
-        }
-
-        for (int i = 0; i < dimension; i++)
-        {
-            for (int j = 0; j < dimension; j++)
-            {
-                cout << intXchar(mySudoku[i][j])<<" ";
+    //print the board
+    void printBoard() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                cout << board[i][j] << " ";
             }
             cout << endl;
         }
     }
 
-    void solveSud()
-    {
-        if (rootLink->right == rootLink)
-        {
-            isSolved = true;
-            if (isSolved){
-                printBoard();
-            return;
+    bool canPlace(int row, int col, int num) {
+        //check row
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num) {
+                return false;
             }
         }
 
-        Root *optimalColumn = rootLink->right;
-
-        for (Root *c =rootLink->right; c != rootLink; c = c->right)
-        {
-            if (c->numOfNodes < optimalColumn->numOfNodes)
-            {optimalColumn = c;}
-
+        //check column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
         }
 
-        _conseal(optimalColumn);
-
-        for (node *row_now = optimalColumn->down; row_now != optimalColumn; row_now = row_now->down)
-        {
-
-            quickSud.push_front(row_now->row);
-
-            for (node *col_now = row_now->right; col_now != row_now; col_now = col_now->right) {
-                _conseal(col_now->head);
+        //check 3x3 box
+        int boxRow = row / 3;
+        int boxCol = col / 3;
+        for (int i = boxRow * 3; i < boxRow * 3 + 3; i++) {
+            for (int j = boxCol * 3; j < boxCol * 3 + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
             }
-            solveSud();
-            quickSud.pop_front();
-
-            for (node *currCol = row_now->left; currCol != row_now; currCol = currCol->left){
-
-                _reveal(currCol->head);
-            }
-
         }
 
-        _reveal(optimalColumn);
+        return true;
     }
 
-private:
-    
-    static void _conseal(Root *col)
-    {
-        col->right->left = col->left;
-        col->left->right = col->right;
+    vector<int> findPlacebles(int row, int col) {
+        vector<int> placebles;
+        for (int i = 1; i <= 9; i++) {
+            if (canPlace(row, col, i)) {
+                placebles.push_back(i);
+            }
+        }
+        return placebles;
+    }
 
-        for (node *r = col->down; r != col; r = r->down)
-        {
-            for (node *c = r->right; c != r; c = c->right)
-            {
-                c->up->down = c->down;
-                c->down->up = c->up;
-                c->head->numOfNodes--;
+    bool SolveSudoku() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == 0) {
+                    vector<int> placebles = findPlacebles(i, j);
+                    for (int k = 0; k < placebles.size(); k++) {
+                        board[i][j] = placebles[k];
+                        if (SolveSudoku()) {
+                            return true;
+                        }
+                        board[i][j] = 0;
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void copyArray(vector<vector<int>> &board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                this->board[i][j] = board[i][j];
             }
         }
     }
 
-    static void _reveal(Root *column)
-    {
+    void nextEmpty(int &row, int &col) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == 0) {
+                    row = i;
+                    col = j;
+                    return;
+                }
+            }
+        }
+    }
 
-        for (node *node_now = column->up; node_now != column; node_now = node_now->up){
-            for (node *_curr = node_now->left; _curr != node_now; _curr = _curr->left){
-                _curr->up->down = _curr;
-                _curr->down->up = _curr;
-                _curr->head->numOfNodes++;
+};
+class Sudoku16x16{
+
+    bool canPlace(int row, int col, int num) {
+        //check row
+        for (int i = 0; i < 16; i++) {
+            if (board[row][i] == num) {
+                return false;
             }
         }
 
-        column->right->left = column;
-        column->left->right = column;
+        //check column
+        for (int i = 0; i < 16; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        //check 4x4 box
+        int boxRow = row / 4;
+        int boxCol = col / 4;
+        for (int i = boxRow * 4; i < boxRow * 4 + 4; i++) {
+            for (int j = boxCol * 4; j < boxCol * 4 + 4; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
+
+    vector<int> findPlacebles(int row, int col) {
+        vector<int> placebles;
+        for (int i = 1; i < 17; i++) {
+            if (canPlace(row, col, i)) {
+                placebles.push_back(i);
+            }
+        }
+        return placebles;
+    }
+
+    bool solve() {
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+                if (board[i][j] == 0) {
+                    vector<int> placebles = findPlacebles(i, j);
+                    for (int k = 0; k < placebles.size(); k++) {
+                        board[i][j] = placebles[k];
+                        if (solve()) {
+                            return true;
+                        }
+                        board[i][j] = 0;
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+public:
+//constructor
+    Sudoku16x16(vector<vector<int>> board) {
+        this->board = board;
+    }
+
+    bool SolveSudoku() {
+        return solve();
+    }
+
+//print the board
+    void printBoard() {
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+//16x16 board
+    vector<vector<int>> board;
+
+    Sudoku16x16() {
+        this->board = vector<vector<int>>(16, vector<int>(16, 0));
+    }
+
+    void printLetterBoard() {
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+                if (board[i][j] == 0) {
+                    cout << "  ";
+                } else {
+                    cout << (char) (board[i][j] + 55) << " ";
+                }
+            }
+            cout << endl;
+        }
+    }
+
 };
 
-static int charxint(char c){
+int main() {
+   /* vector<vector<int>> board = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };*/
 
-    if((int)c<58 && (int)c>47){
+    Sudoku9x9 sudoku;
 
-        return (int)c - 48;
-    }
-    else{
-
-        return (int)c - 55;
-    }
-}
-
-int main()
-{
-    string line;
-
-    for (int i =0; i<16;++i)
-    {
-        vector<int>  row;
-        getline(cin, line);
-
-        stringstream currLine(line);
-
-        string rowToAdd;
-
-        while (getline(currLine, rowToAdd, ' ')){
-          row.push_back(charxint(rowToAdd[0]));
-        }
-        mySudoku.push_back(row);
-    }
-
-
-    Sudoku16x16 mySudoku16x16;
-    mySudoku16x16.solveSud();
-
-    if (!isSolved){
+    sudoku.takeInput();
+    if(sudoku.SolveSudoku()) {
+        sudoku.printBoard();
+    } else {
         cout << "No Solution" << endl;
     }
+
     return 0;
 }
 
+
+/*Sample input:
+5 3 0 0 7 0 0 0 0
+6 0 0 1 9 5 0 0 0
+0 9 8 0 0 0 0 6 0
+8 0 0 0 6 0 0 0 3
+4 0 0 8 0 3 0 0 1
+7 0 0 0 2 0 0 0 6
+0 6 0 0 0 0 2 8 0
+0 0 0 4 1 9 0 0 5
+0 0 0 0 8 0 0 7 9
+
+Sample Output:
+5 3 4 6 7 8 9 1 2
+6 7 2 1 9 5 3 4 8
+1 9 8 3 4 2 5 6 7
+8 5 9 7 6 1 4 2 3
+4 2 6 8 5 3 7 9 1
+7 1 3 9 2 4 8 5 6
+9 6 1 5 3 7 2 8 4
+2 8 7 4 1 9 6 3 5
+3 4 5 2 8 6 1 7 9
+
+*/
